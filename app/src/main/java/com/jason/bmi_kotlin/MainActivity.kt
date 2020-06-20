2667a39410ceb0b2880e4a1aa988003a90874949
jason.chien <Jason_Chien@quantatw.com>
2020-06-20 16:05:56 +0800

    linear layout

--- app/src/main/java/com/jason/bmi_kotlin/MainActivity.kt
+++ app/src/main/java/com/jason/bmi_kotlin/MainActivity.kt
@@ -2,11 +2,23 @@ package com.jason.bmi_kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+    companion object {
+        private val TAG = "BMI"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun bmi(view : View) {
+        val weight = ed_weight.text.toString().toFloat()
+        val height = ed_height.text.toString().toFloat()
+        val bmi = weight  / (height*height)
+        Log.d(TAG, "Your BMI is $bmi")
     }
 }
