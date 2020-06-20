f65bc663cc89b323da3287e5177fddc0547d7c76
jason.chien <Jason_Chien@quantatw.com>
2020-06-20 16:18:53 +0800

    Toast, AlertDialog

--- app/src/main/java/com/jason/bmi_kotlin/MainActivity.kt
+++ app/src/main/java/com/jason/bmi_kotlin/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +22,11 @@ class MainActivity : AppCompatActivity() {
         val height = ed_height.text.toString().toFloat()
         val bmi = weight  / (height*height)
         Log.d(TAG, "Your BMI is $bmi")
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setTitle("Your BMI")
+            .setPositiveButton("OK", null)
+            .show()
     }
 }
